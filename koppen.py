649694815d2a35c1f6d2885deb024d2fbf02e1aa
Patrick W. Zimmeman <pzimmerman@epcw.org>
2022-04-29 17:31:03 -0700

    Restored link to dl koppen_class.csv after de-duping the file so it's 60MB not 1.4GB (this was done programmatically in data_prep.py before but this saves space).

--- koppen.py
+++ koppen.py
@@ -266,4 +266,10 @@ print("assigning Köppen classes")
 
 # #IF EXISTING
 df_agg.to_csv(df_filename, index=False, quotechar='"', mode="a", quoting=csv.QUOTE_ALL, header=False)
+
+# check and de-duplicate
+df_check = pd.read_csv(df_filename)
+df_check = df_check.drop_duplicates()
+
+df_check.to_csv(df_filename, index=False, quotechar='"', quoting=csv.QUOTE_ALL, header=True)
 print("exporting " + df_filename)
